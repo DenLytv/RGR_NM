53ce84b750b6ee78008b606905d582941b5c2d5c
Den <denys.lytvynenko2208@gmail.com>
2025-05-24 00:15:27 +0300

    Add project files.

---
+++ RGR_NM/Program.cs
@@ -0,0 +1,69 @@
+﻿//double[] X = { 0.4, 0.8, 1.2, 1.6, 2 };
+double[] X = { 2, 4, 6, 8, 10 };
+//double[] Y = { -0.916291, -0.223144, 0.182322, 0.47004, 0.693147 };
+double[] Y = { 0.693147, 1.38629, 1.79176, 2.07944, 2.30259 };
+
+Y = fillY(X);
+
+double x = 3;
+//double x = 0.5;
+
+double result = NewtonBackwardInterpolation(X, Y, x);
+Console.WriteLine($"The value of a function at a point x = {x}: {result}");
+
+double absoluteError = Math.Abs(Math.Log(x) - result);
+Console.WriteLine($"Absolute error: {absoluteError}");
+Console.WriteLine($"Relative error: {absoluteError / Math.Abs(Math.Log(x)) * 100}%");
+
+double[] fillY(double[] X)
+{
+    double[] tmp = new double[X.Length];
+    for (int i = 0; i < X.Length; i++)
+    {
+        tmp[i] = Math.Log(X[i]);
+    }
+    return tmp;
+}
+
+double[,] CalculateBackwardDifferences(double[] Y)
+{
+    int n = Y.Length;
+    double[,] differences = new double[n, n];
+
+    for (int i = 0; i < n; i++)
+        differences[i, 0] = Y[i];
+
+    for (int j = 1; j < n; j++)
+        for (int i = n - 1; i >= j; i--)
+            differences[i, j] = differences[i, j - 1] - differences[i - 1, j - 1];
+
+    return differences;
+}
+
+double Factorial(int n)
+{
+    double result = 1;
+    for (int i = 2; i <= n; i++)
+        result *= i;
+    return result;
+}
+
+double NewtonBackwardInterpolation(double[] X, double[] Y, double x)
+{
+    int n = X.Length;
+    double[,] differences = CalculateBackwardDifferences(Y);
+
+    double h = X[1] - X[0];
+    double result = Y[n - 1];
+
+    double termMultiplier = 1.0;
+
+    for (int i = 1; i <= 4; i++)
+    {
+        termMultiplier *= (x - X[n - i]);
+        double term = (termMultiplier / (Factorial(i) * Math.Pow(h, i))) * differences[n - 1, i];
+        result += term;
+    }
+
+    return result;
+}
---
+++ SLAR/Program.cs
@@ -0,0 +1,119 @@
+﻿double[,] A = {
+    { -11, 1, -6, -3 },
+    { -6, 15, -4, 3 },
+    { 1, 1, 11, 1 },
+    { -4, 1, 1, -11 }
+};
+double[] B = { -9, 4, -2, 4 };
+
+double[] solutionKhaletskyScheme = ModifiedKhaletskyScheme(A, B);
+double[] solutionGaussSeidel = GaussSeidel(A, B, 3);
+
+Console.WriteLine("Solution by the modified Gaussian method according to the Khaletskyi scheme:");
+for (int i = 0; i < solutionKhaletskyScheme.Length; i++)
+{
+    Console.WriteLine($"x[{i + 1}] = {solutionKhaletskyScheme[i]}");
+}
+
+Console.WriteLine("\nSolution by the Gauss-Seidel method (3 iterations):");
+for (int i = 0; i < solutionGaussSeidel.Length; i++)
+{
+    Console.WriteLine($"x[{i + 1}] = {solutionGaussSeidel[i]}");
+}
+
+CalculateErrors(solutionKhaletskyScheme, solutionGaussSeidel);
+
+double[] ModifiedKhaletskyScheme(double[,] A, double[] B)
+{
+    int n = A.GetLength(0);
+    double[,] C = new double[n, n];
+    double[,] D = new double[n, n];
+
+    for (int i = 0; i < n; i++)
+        D[i, i] = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            C[j, i] = A[j, i];
+            for (int k = 0; k < i; k++)
+            {
+                C[j, i] -= C[j, k] * D[k, i];
+            }
+        }
+
+        for (int j = i + 1; j < n; j++)
+        {
+            D[i, j] = A[i, j];
+            for (int k = 0; k < i; k++)
+            {
+                D[i, j] -= C[i, k] * D[k, j];
+            }
+            D[i, j] /= C[i, i];
+        }
+    }
+
+    double[] y = new double[n];
+    for (int i = 0; i < n; i++)
+    {
+        y[i] = B[i];
+        for (int k = 0; k < i; k++)
+        {
+            y[i] -= C[i, k] * y[k];
+        }
+        y[i] /= C[i, i];
+    }
+
+    double[] x = new double[n];
+    x[n - 1] = y[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        x[i] = y[i];
+        for (int k = i + 1; k < n; k++)
+        {
+            x[i] -= D[i, k] * x[k];
+        }
+    }
+    return x;
+}
+
+double[] GaussSeidel(double[,] A, double[] B, int iterations)
+{
+    int n = A.GetLength(0);
+    double[] x = new double[n];
+
+    for (int iteration = 0; iteration < iterations; iteration++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            double sum = B[i];
+            for (int j = 0; j < n; j++)
+            {
+                if (j != i)
+                {
+                    sum -= A[i, j] * x[j];
+                }
+            }
+            x[i] = sum / A[i, i];
+        }
+    }
+    return x;
+}
+
+void CalculateErrors(double[] exactSolution, double[] approxSolution)
+{
+    Console.WriteLine("Absolute errors:");
+    for (int i = 0; i < exactSolution.Length; i++)
+    {
+        double absoluteError = Math.Abs(exactSolution[i] - approxSolution[i]);
+        Console.WriteLine($"Absolute error for x[{i + 1}] = {absoluteError}");
+    }
+
+    Console.WriteLine("Relative errors:");
+    for (int i = 0; i < exactSolution.Length; i++)
+    {
+        double relativeError = Math.Abs((exactSolution[i] - approxSolution[i]) / exactSolution[i]);
+        Console.WriteLine($"Relative error for x[{i + 1}] = {relativeError * 100}%");
+    }
+}
